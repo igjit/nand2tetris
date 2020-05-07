7cc691fb980a2cc1279f869a7a5c7227a195f3be
igjit <igjit1@gmail.com>
2020-05-07 11:21:55 +0900

    Move constructors to assemble.R

--- r/assembler/R/parse.R
+++ r/assembler/R/parse.R
@@ -41,11 +41,3 @@ parse_c_command <- function(string) {
 
   c_command(dest, comp, jump)
 }
-
-a_command <- function(int) {
-  structure(list(int = int), class = "a_command")
-}
-
-c_command <- function(dest = NULL, comp = NULL, jump = NULL) {
-  structure(list(dest = dest, comp = comp, jump = jump), class = "c_command")
-}
