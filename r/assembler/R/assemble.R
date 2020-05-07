075a91a9deedae46f7dc51476baac0832db642c3
igjit <igjit1@gmail.com>
2020-05-07 14:36:55 +0900

    Implement assemble.c_command

--- r/assembler/R/assemble.R
+++ r/assembler/R/assemble.R
@@ -4,6 +4,61 @@ assemble.a_command <- function(x) {
   str_c("0", as_bin(x$int, 15), collapse = "")
 }
 
+assemble.c_command <- function(x) {
+  str_c("111",
+        assemble_c_comp(x$comp),
+        assemble_c_dest(x$dest),
+        assemble_c_jump(x$jump),
+        collapse = "")
+}
+
+assemble_c_comp <- function(comp) {
+  switch(comp,
+         "0" = "0101010",
+         "1" = "0111111",
+         "-1" = "0111010",
+         "D" = "0001100",
+         "A" = "0110000",
+         "!D" = "0001101",
+         "!A" = "0001101",
+         "-D" = "0001111",
+         "-A" = "0110011",
+         "D+1" = "0011111",
+         "A+1" = "0110111",
+         "D-1" = "0001110",
+         "A-1" = "0110010",
+         "D+A" = "0000010",
+         "D-A" = "0010011",
+         "A-D" = "0000111",
+         "D&A" = "0000000",
+         "D|A" = "0010101",
+         "M" = "1110000",
+         "!M" = "1001101",
+         "-M" = "1110011",
+         "M+1" = "1110111",
+         "M-1" = "1110010",
+         "D+M" = "1000010",
+         "D-M" = "1010011",
+         "M-D" = "1000111",
+         "D&M" = "1000000",
+         "D|M" = "1010101",
+         stop("Not implemented: ", comp))
+}
+
+assemble_c_dest <- function(dest) {
+  if (is.null(dest)) dest <- ""
+  contains <- c("A", "D", "M") %>%
+    map_chr(~ str_match(dest, .)[1, 1])
+  ifelse(is.na(contains), "0", "1") %>%
+    str_c(collapse = "")
+}
+
+assemble_c_jump <- function(jump) {
+  if (is.null(jump)) return("000")
+  which(c("JGT", "JEQ", "JGE", "JLT", "JNE", "JLE", "JMP") == jump) %>%
+    as_bin(3)
+}
+
 as_bin <- function(int, digit) {
   intToBits(int) %>%
     head(digit) %>%
--- r/assembler/tests/testthat/test-assemble.R
+++ r/assembler/tests/testthat/test-assemble.R
@@ -1,3 +1,24 @@
 test_that("assemble.a_command works", {
   expect_equal(assemble(a_command(2)), "0000000000000010")
 })
+
+test_that("assemble.c_command works", {
+  expect_equal(assemble(parse_c_command("D=D+A")), "1110000010010000")
+  expect_equal(assemble(parse_c_command("0;JMP")), "1110101010000111")
+})
+
+test_that("assemble_c_comp works", {
+  expect_equal(assemble_c_comp("D+1"), "0011111")
+})
+
+test_that("assemble_c_dest works", {
+  expect_equal(assemble_c_dest(NULL), "000")
+  expect_equal(assemble_c_dest("D"), "010")
+  expect_equal(assemble_c_dest("AM"), "101")
+})
+
+test_that("assemble_c_jump works", {
+  expect_equal(assemble_c_jump(NULL), "000")
+  expect_equal(assemble_c_jump("JGT"), "001")
+  expect_equal(assemble_c_jump("JLE"), "110")
+})
